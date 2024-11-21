f5de8d3d498d1274a079690d9d0f0ac4285621d5
babicaewa <alexisanepicgamer@gmail.com>
2024-11-21 08:44:54 -0500

    minor comment changes

--- compare_files_test.py
+++ compare_files_test.py
@@ -15,14 +15,14 @@ downloaded_files_folder = os.path.join(os.path.dirname(os.path.realpath(__file__
 
 #testing purposes, change directories as needed
 
-#file1 = "/Users/Alex/Downloads/debian-mac-12.7.0-amd64-netinst.iso(fromthesource).iso"             #debian os ~670mb
-#file2 = os.path.join(downloaded_files_folder, "debian-mac-12.7.0-amd64-netinst.iso")
+file1 = "/Users/Alex/Downloads/debian-mac-12.7.0-amd64-netinst.iso(fromthesource).iso"             #debian os ~660mb
+file2 = os.path.join(downloaded_files_folder, "debian-mac-12.7.0-amd64-netinst.iso")
 
 #file1 = "/Users/Alex/Downloads/2024-11-13-raspios-bookworm-armhf-lite(fromthesource).img.xz"    
 #file2 = os.path.join(downloaded_files_folder, "2024-11-13-raspios-bookworm-armhf-lite.img.xz")    #raspberry pi lite os ~535mb
 
-file1 = "/Users/Alex/Downloads/2024-11-13-raspios-bookworm-armhf-full(fromthesource).img.xz"      #full raspberry pi os with desktop & recommended software ~2.86gb
-file2 = os.path.join(downloaded_files_folder, "2024-11-13-raspios-bookworm-armhf-full.img.xz") 
+#file1 = "/Users/Alex/Downloads/2024-11-13-raspios-bookworm-armhf-full(fromthesource).img.xz"      #full raspberry pi os with desktop & recommended software ~2.86gb
+#file2 = os.path.join(downloaded_files_folder, "2024-11-13-raspios-bookworm-armhf-full.img.xz") 
 
 
 if calculate_file_hash(file1) == calculate_file_hash(file2):
